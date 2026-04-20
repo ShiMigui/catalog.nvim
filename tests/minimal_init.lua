9f6139fff1b0b458bed9b16b708e48c502d7e80d
Miguel Nascimento <miguel.nasto@gmail.com>
2026-04-20 18:01:53 -0300

    chore: remove legacy tests

--- tests/minimal_init.lua
+++
@@ -1,8 +0,0 @@
-vim.opt.rtp:append(vim.fn.stdpath("data") .. "/lazy/plenary.nvim")
-local root = vim.fn.getcwd()
-
-package.path = package.path .. ";" .. root .. "/tests/?.lua"
-package.path = package.path .. ";" .. root .. "/tests/?/init.lua"
-
-require("plenary.busted")
-vim.cmd("PlenaryBustedDirectory tests/specs")
