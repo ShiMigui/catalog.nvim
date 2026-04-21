723c4c2999499624a2b1e4926d697c2f04559ce1
Miguel Nascimento <miguel.nasto@gmail.com>
2026-04-21 20:47:50 -0300

    refactor: trying to make it work

--- lua/mason-catalog.lua
+++ lua/mason-catalog.lua
@@ -1,42 +1,54 @@
 local scope = ...
 
-local setup_opts
-local function setup()
-	vim.g.mason_catalog_debug = false
-	vim.g.mason_catalog_silent = false
+local function setup(opts)
+	vim.g.mason_catalog_debug = opts.debug == true
+	vim.g.mason_catalog_silent = opts.silent == true
+
 	local logger = require("mason-catalog.logger").logger(scope)
 	local provider = require("mason-catalog.core.provider")
+
 	logger.dbg("starting")
 
-	if setup_opts.lsp then
-		local lsp = require("mason-catalog.core.lsp")
-		lsp(setup_opts.lsp)
+	if opts.lsp then
+		require("mason-catalog.core.lsp")(opts.lsp)
 	end
 
-	for _, pkg in ipairs(setup_opts.ensure_installed or {}) do
-		local p = provider.resolve(pkg)
-		if p then
-			p.install()
+	-- ensure_installed
+	local seen = {}
+	for _, pkg in ipairs(opts.ensure_installed or {}) do
+		if not seen[pkg] then
+			seen[pkg] = true
+
+			local p = provider.resolve(pkg)
+			if p then
+				p.install()
+			else
+				logger.err("Package '%s' not found", pkg)
+			end
 		end
 	end
 
-	for _, integration in ipairs(setup_opts.integrations or {}) do
-		local p = logger.try_require("mason-catalog.integrations." .. integration)
-		if p then
-			p()
+	-- integrations
+	for _, integration in ipairs(opts.integrations or {}) do
+		local mod = logger.try_require("mason-catalog.integrations." .. integration)
+		if type(mod) == "function" then
+			mod()
 		end
 	end
 end
 
 return {
 	setup = function(opts)
-		setup_opts = opts or {}
+		opts = opts or {}
+
 		local registry = require("mason-registry")
 
-		if #registry.get_all_packages() > 0 then
-			setup()
+		if registry.get_all_packages and registry.get_all_packages() > 0 then
+			setup(opts)
 		else
-			registry.refresh(setup)
+			registry.refresh(function()
+				setup(opts)
+			end)
 		end
 	end,
 }
