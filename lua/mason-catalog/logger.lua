0a12f47548b0a24d374d8468ac7a9df19c0d88a1
Miguel Nascimento <miguel.nasto@gmail.com>
2026-04-20 22:16:35 -0300

    refactor: update logger module

--- lua/mason-catalog/logger.lua
+++ lua/mason-catalog/logger.lua
@@ -1,11 +1,6 @@
 ---@alias LogFunction fun(msg: string, ...: any): nil
 
-local function format(scope, msg, ...)
-	if select("#", ...) > 0 then
-		msg = msg:format(...)
-	end
-	return scope .. msg
-end
+local format = require("mason-catalog.utils.format")
 
 ---@param scope string
 ---@param level vim.log.levels
@@ -40,24 +35,24 @@ local fn_ic_silent = vim.g.mason_catalog_silent and return_noop or notify_scope
 local fn_ic_debug = vim.g.mason_catalog_debug and notify_scope or return_noop
 local levels = vim.log.levels
 
-return {
-	---@param scope string
-	scope = function(scope)
-		scope = "[" .. scope .. "] "
+---@param scope string
+local function logger(scope)
+	scope = scope and "[" .. scope .. "] " or ""
 
-		local logger = {
-			err = fn_ic_silent(scope, levels.ERROR),
-			inf = fn_ic_silent(scope, levels.INFO),
-			wrn = fn_ic_silent(scope, levels.WARN),
-			dbg = fn_ic_debug(scope, levels.DEBUG),
-			error = function(msg, ...)
-				error(format(scope, msg, ...), 2)
-			end,
-		}
+	local m = {
+		err = fn_ic_silent(scope, levels.ERROR),
+		inf = fn_ic_silent(scope, levels.INFO),
+		wrn = fn_ic_silent(scope, levels.WARN),
+		dbg = fn_ic_debug(scope, levels.DEBUG),
+		error = function(msg, ...)
+			error(format(scope, msg, ...), 2)
+		end,
+	}
 
-		logger.require = require_builder(logger.dbg, logger.error)
-		logger.try_require = require_builder(logger.dbg, logger.err)
+	m.require = require_builder(m.dbg, m.error)
+	m.try_require = require_builder(m.dbg, m.err)
 
-		return logger
-	end,
-}
+	return m
+end
+
+return logger
