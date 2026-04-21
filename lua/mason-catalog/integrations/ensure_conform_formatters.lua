c783dfcadceb3415d8342005621c27cd3798277a
Miguel Nascimento <miguel.nasto@gmail.com>
2026-04-21 20:37:31 -0300

    refactor: integration command verify

--- lua/mason-catalog/integrations/ensure_conform_formatters.lua
+++ lua/mason-catalog/integrations/ensure_conform_formatters.lua
@@ -3,16 +3,18 @@ local provider = require("mason-catalog.core.provider")
 local mod = logger.require("conform")
 
 return function()
-  logger.inf("starting")
-  local seen = {}
-  for _, fmt in ipairs(mod.list_all_formatters()) do
-    local nm = fmt.command or fmt.name
-    if not seen[nm] then
-      local p  = provider.resolve(nm)
-      seen[nm] = p or false
-      if p then
-        p.install()
-      end
-    end
-  end
+	logger.inf("starting")
+	local seen = {}
+
+	for _, f in ipairs(mod.list_all_formatters()) do
+		local nm = f.command
+
+		if nm and not seen[nm] then
+			seen[nm] = true
+			local p = provider.resolve(nm)
+			if p then
+				p.install()
+			end
+		end
+	end
 end
