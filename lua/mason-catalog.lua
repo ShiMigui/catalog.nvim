92a292abf839efaa509c6bbe3f88538d5e6dede9
Miguel Nascimento <miguel.nasto@gmail.com>
2026-04-21 20:09:46 -0300

    chore: update mason-catalog main file

--- lua/mason-catalog.lua
+++ lua/mason-catalog.lua
@@ -1,7 +1,24 @@
+local scope = ...
+
+local function setup(opts)
+	vim.g.mason_catalog_debug = false
+	vim.g.mason_catalog_silent = false
+	local logger = require("mason-catalog.logger").logger(scope)
+	logger.dbg("Starting setup")
+
+	if opts.lsp then
+		local lsp = require("mason-catalog.core.lsp")
+		lsp(opts.lsp)
+	end
+
+	vim.opt.rtp:prepend(vim.fn.getcwd())
+end
+
 return {
-	setup = function()
-		vim.cmd("LspStart lua_ls")
-		vim.opt.rtp:prepend(vim.fn.getcwd())
-		print("IT IS RUNNING")
+	setup = function(opts)
+		opts = opts or {}
+		setup({
+			lsp = { extensions = { { "lua", lsp = { "lua-language-server", "emmylua_ls" } } } },
+		})
 	end,
 }
