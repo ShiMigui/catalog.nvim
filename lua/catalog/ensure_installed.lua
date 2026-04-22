09d059b718637243f20e75e29b6196195db904b4
Miguel Nascimento <miguel.nasto@gmail.com>
2026-04-22 19:39:55 -0300

    feat: add ensure installed integration

---
+++ lua/catalog/ensure_installed.lua
@@ -0,0 +1,22 @@
+local provider = require("catalog.provider")
+local log = require("catalog.log").log(...)
+
+---@type catalog.integration
+return {
+	init = function(opts)
+		if type(opts) == "string" then
+			opts = { opts }
+		elseif not vim.islist(opts) then
+			log.wrn("There is no packages to ensure installation in ensure_install")
+			return
+		end
+
+		---@diagnostic disable-next-line: param-type-mismatch
+		for _, pkg in ipairs(opts) do
+			local p = provider.resolve(pkg)
+			if p and not p.installed() then
+				p.install()
+			end
+		end
+	end,
+}
