a344f175025e89c842879aa5ba82923a48b4c102
Miguel Nascimento <miguel.nasto@gmail.com>
2026-04-22 10:09:49 -0300

    refactor: change autocmd for filetypes opts

--- lua/mason-catalog/core/lsp/init.lua
+++ lua/mason-catalog/core/lsp/init.lua
@@ -2,7 +2,6 @@ local logger = require("mason-catalog.logger").logger(...)
 local lsp_entry = require("mason-catalog.normalize.lsp_entry")
 local mapper = require("mason-catalog.core.lsp.mapper")
 local ext_to_ft = require("mason-catalog.normalize.ext")
-local config_autocmd = require("mason-catalog.core.lsp.config_autocmd")
 
 local identity = function(x)
 	return x
@@ -14,13 +13,10 @@ return function(opts)
 
 	opts = lsp_entry(opts)
 
-	local map = {}
-	local ctx = { map = map, default_config = opts.default_config, transform = identity }
+	local ctx = { default_config = opts.default_config, transform = identity }
 
 	mapper.try_run(opts, "filetypes", ctx)
 
 	ctx.transform = ext_to_ft
 	mapper.try_run(opts, "extensions", ctx)
-
-	config_autocmd(map)
 end
--- lua/mason-catalog/core/lsp/mapper.lua
+++ lua/mason-catalog/core/lsp/mapper.lua
@@ -1,5 +1,6 @@
 local logger = require("mason-catalog.logger").logger(...)
 local lsp_spec = require("mason-catalog.normalize.lsp_spec")
+local provider = require("mason-catalog.core.provider")
 
 ---@class lsp.run.ctx
 ---@field map table<string, cat.pkg.name[]>
@@ -18,35 +19,48 @@ local function run(group, ctx)
 		return
 	end
 
+	local fts = {}
 	for _, k in ipairs(group) do
-		k = ctx.transform(k)
-		if ctx.map[k] then
-			logger.err("LSP already configured, key = '%s'", k)
-		else
-			ctx.map[k] = specs
+		fts[#fts + 1] = ctx.transform(k)
+	end
+
+	for _, pkg_name in ipairs(specs) do
+		local p = provider.resolve(pkg_name)
+		if p then
+			local name = p.lspname
+			if not p.installed then
+				logger.wrn("LSP '%s' will not be enabled, because it's not installedd yet", name)
+				return
+			end
+
+			if not vim.lsp.is_enabled(name) then
+				logger.dbg("activating %s", name)
+				vim.lsp.config(name, vim.tbl_deep_extend("force", p.lspconfig, { filetypes = fts }))
+				vim.lsp.enable(name)
+			end
 		end
 	end
 end
 
----@param opts cat.opts.lsp
----@param name "filetypes"|"extensions"
----@param ctx lsp.run.ctx
-local function try_run(opts, name, ctx)
-	logger.dbg("trying")
-	local groups = opts[name]
-	if #groups == 0 then
-		return
-	end
-
-	logger.dbg("Configuring lsp.%s", name)
-	for _, g in ipairs(groups) do
-		local tg = type(g)
-		if tg ~= "table" then
-			logger.err("Group is not a table, got '%s'. Exiting", tg)
+return {
+	---@param opts cat.opts.lsp
+	---@param name "filetypes"|"extensions"
+	---@param ctx lsp.run.ctx
+	try_run = function(opts, name, ctx)
+		logger.dbg("trying")
+		local groups = opts[name]
+		if #groups == 0 then
 			return
 		end
-		run(g, ctx)
-	end
-end
 
-return { try_run = try_run }
+		logger.dbg("Configuring lsp.%s", name)
+		for _, g in ipairs(groups) do
+			local tg = type(g)
+			if tg ~= "table" then
+				logger.err("Group is not a table, got '%s'. Exiting", tg)
+				return
+			end
+			run(g, ctx)
+		end
+	end,
+}
