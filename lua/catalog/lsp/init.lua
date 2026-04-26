3a07d1c94a801ae1c47267e205c344855653c63e
Miguel Nascimento <miguel.nasto@gmail.com>
2026-04-25 22:04:37 -0300

    refactor(LSP): add registry and process suport on init.lua

--- lua/catalog/lsp/init.lua
+++ lua/catalog/lsp/init.lua
@@ -1,9 +1,17 @@
-local configure_spec = require("catalog.lsp.configure_spec")
+local registry = require("catalog.lsp.registry")
+local process = require("catalog.lsp.process")
 local log = require("catalog.log").log(...)
 
 ---@type catalog.integration
 return {
-	---@param opts any|catalog.entry.lsp
+	---Entry point for catalog LSP integration.
+	---
+	---Responsibilities:
+	---- Normalize user input (array + map styles)
+	---- Process all specs
+	---- Enable all configured LSPs
+	---
+	---@param opts catalog.entry.lsp
 	init = function(opts)
 		log.header(true)
 		if type(opts) ~= "table" then
@@ -12,27 +20,16 @@ return {
 		end
 
 		local config = opts.config or { capabilities = vim.lsp.protocol.make_client_capabilities() }
-
-		local map = {}
-
 		opts.config = nil
-		for i, spec in ipairs(opts) do
-			configure_spec(i, spec, config, map)
-			opts[i] = nil
-		end
-		for ft, lsp in pairs(opts) do
-			configure_spec(ft, { ft, lsp = lsp }, config, map)
+		for i, spec in pairs(opts) do
+			if type(i) == "string" then
+				process(i, { i, lsp = spec }, config)
+			else
+				process(i, spec, config)
+			end
 		end
 
-		for name, m in pairs(map) do
-			local lsp = m[3]
-			local fts = m[2]
-
-			lsp.update({ filetypes = fts })
-			vim.lsp.config(lsp, lsp.config)
-			vim.lsp.enable(lsp)
-			log.dbg("%s is running", name)
-		end
+		registry.enable_all()
 		log.header(false)
 	end,
 }
