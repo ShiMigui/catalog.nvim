f04c72f7a72c63273ce5cf4e6c303fded81e2a9b
Miguel Nascimento <miguel.nasto@gmail.com>
2026-06-05 21:44:51 -0300

    refactor: turn file easier to read

--- lua/catalog/lsp/config.lua
+++ lua/catalog/lsp/config.lua
@@ -1,10 +1,17 @@
+---@alias catalog.capability_provider
+---| "blink.cmp"
+---| "nvim-cmp"
+---
 ---@class catalog.entry.lsp.config
 ---@field config? catalog.lsp.config
----@field capabilities? "nvim-cmp"|"blink.cmp"
+---@field capabilities? catalog.capability_provider
 
 local log = require("catalog.log").log(...)
-local lsp_default_config = {}
 
+---@type catalog.lsp.config
+local lsp_default_config = { capabilities = vim.lsp.protocol.make_client_capabilities() }
+
+---@type table<string, fun(): table>
 local capability_providers = {
 	["blink.cmp"] = function()
 		return require("blink.cmp").get_lsp_capabilities()
@@ -14,46 +21,44 @@ local capability_providers = {
 	end,
 }
 
----@param provider? string
----@return table
-local function make_capabilities(provider)
-	local default = vim.lsp.protocol.make_client_capabilities()
-
-	if not provider then
-		log.dbg("Returning default capabilities, no provider given")
-		return default
-	end
-	log.dbg("Trying to load capabilities from provider '%s'", provider)
-
-	local fn = capability_providers[provider]
-
-	if not fn then
+---@param provider string
+---@return nil
+local function apply_provider_capabilities(provider)
+	log.dbg("Loading capabilities from '%s'", provider)
+	local loader = capability_providers[provider]
+	if not loader then
 		log.err("Unknown capability provider: %s", provider)
-		return default
+		return
 	end
 
-	local ok, capabilities = pcall(fn)
-
+	local ok, capabilities = pcall(loader)
 	if not ok then
 		log.err("Failed to load capability provider '%s': %s", provider, capabilities)
-		return default
+		return
 	end
 
-	log.dbg("Loaded capabilities from provider '%s'", provider)
-	return vim.tbl_deep_extend("force", default, capabilities)
+	log.dbg("Capabilities loaded from '%s'", provider)
+	lsp_default_config.capabilities = vim.tbl_deep_extend("force", lsp_default_config.capabilities, capabilities)
 end
 
 ---@type catalog.integration
 return {
-	---@param opts catalog.entry.lsp.config
+	---@param opts? catalog.entry.lsp.config
 	setup = function(opts)
 		if not opts then
 			return
 		end
 
-		lsp_default_config =
-			vim.tbl_deep_extend("force", { capabilities = make_capabilities(opts.capabilities) }, opts.config or {})
+		if type(opts.capabilities) == "string" then
+			apply_provider_capabilities(opts.capabilities)
+		end
+
+		if type(opts.config) == "table" then
+			lsp_default_config = vim.tbl_deep_extend("force", lsp_default_config, opts.config)
+		end
 	end,
+
+	---@return catalog.lsp.config
 	config = function()
 		return lsp_default_config
 	end,
